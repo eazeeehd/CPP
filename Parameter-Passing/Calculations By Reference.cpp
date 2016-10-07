5d297dab3f3ea17dffa5b60b174771ed7b9d062b
Peter Kemley <eazeeehd@hotmail.com>
2016-10-07 10:13:40 +0100

    Update and rename Reference.cpp to Calculations By Reference.cpp

--- Parameter-Passing/Calculations By Reference.cpp
+++ Parameter-Passing/Calculations By Reference.cpp
@@ -12,9 +12,6 @@ void subtract(int num1_in, int num2_in, double & answer_out);
 void multiply(int num1_in, int num2_in, double & answer_out);
 void divide(int num1_in, int num2_in, double & answer_out);
 
-//global variables
-
-
 int _tmain(int argc, _TCHAR* argv[])
 {
 	system ("cls");
@@ -25,11 +22,11 @@ int _tmain(int argc, _TCHAR* argv[])
 void input_output()
 {	
 	////////////////////////////////////////////////////////        //////////////////////////////////////////////////////// 
-	//Takes 2 numbers - user prompted to enter            //        //add 57+24 = 81									 //
-	//initially hard coded								  //        //subtract 57-24 = 33			TESTDATA		    //
-	//call a method to process numbers					  //		//multiply 57*24 = 1368							   //
-	//user will be asked to chose what process they want  //		//divide 57/24 = 2.375							  //
-	////////////////////////////////////////////////////////		///////////////////////////////////////////////////
+	//Takes 2 numbers - user prompted to enter            //        //add 57+24 = 81				     //				 //
+	//initially hard coded				      //        //subtract 57-24 = 33			TESTDATA    //
+	//call a method to process numbers		      //	//multiply 57*24 = 1368				   //
+	//user will be asked to chose what process they want  //	//divide 57/24 = 2.375				  //
+	////////////////////////////////////////////////////////	///////////////////////////////////////////////////
 	int num1, num2;
 	double answer;
 	int user;
@@ -66,12 +63,16 @@ void input_output()
 		cout << num1 << " x " << num2 << endl;
 		cout << "The Result is: " << answer << endl;
 	}
-	else
+	else if (user == 4)
 	{
 		//divide
 		divide(num1, num2, answer);
 		cout << num1 << " / " << num2 << endl;
 		cout << "The Result is: " << answer << endl;
+	}
+	else
+	{
+	    cout << "Your Selection was not an option please try again" << endl;	
 	}
 }
 
